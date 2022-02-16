041b237444e3bb2bb932a100e42fecd93ec37b29
VallentinDS <62264486+VallentinDS@users.noreply.github.com>
2022-02-16 16:44:38 +0100

    Add files via upload

---
+++ GMP_scrapper.py
@@ -0,0 +1,83 @@
+var IntegrationDetails = [{
+    property1 = {}
+}]
+
+IntegrationDetails[0]["Property Name"] = document.querySelectorAll('mat-row')[0].innerText
+
+IntegrationDetails[0]["Advertiser"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-entityCountText.mat-column-entityCountText.ng-star-inserted > span").innerText
+
+IntegrationDetails[0]["Tracking ID"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-id.mat-column-id.ng-star-inserted > span").innerText
+
+IntegrationDetails[0]["Account"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-parent.mat-column-parent.ng-star-inserted > span").innerText
+
+IntegrationDetails[0]["Organisation name"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-parent.mat-column-parent.ng-star-inserted > span").innerText
+
+IntegrationDetails[0]["Integrations with reporting data"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.ic-icon-cell.cdk-column-feature0.mat-column-feature0.ng-star-inserted > span").innerText
+
+IntegrationDetails[0]["Integrations with Cost Data"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.ic-icon-cell.cdk-column-feature0.mat-column-feature0.ng-star-inserted > span").innerText
+
+IntegrationDetails[0]["Integrations with Remarketing Lists"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.ic-icon-cell.cdk-column-feature2.mat-column-feature2.ng-star-inserted > span").innerText
+
+copy(IntegrationDetails)
+
+## si daca o sa aibe ceva de genul const 
+
+mathFields = document.querySelectorAll('mat-row')
+
+## Dupa ar putea face ceva de genul
+
+mathFields.forEach(key => {
+dictionary[key].push(mathFields[key]
+});
+
+
+[
+    {
+        "Property Name": "ellos.dk",
+        "Advertiser": "2 advertisers",
+        "Tracking ID": "UA-16531570-11",
+        "Account": "Ellos",
+        "Organisation name": "Ellos",
+        "Integrations with reporting data": "2",
+        "Integrations with Cost Data": "2",
+        "Integrations with Remarketing Lists": "2"
+    }
+]
+
+var IntegrationDetails = [{
+    property1 = {}
+}]
+i=0;
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[i]["Property Name"] = document.querySelectorAll('mat-row')[i].innerText
+}
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Advertiser"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-entityCountText.mat-column-entityCountText.ng-star-inserted > span").innerText
+}
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Tracking ID"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-id.mat-column-id.ng-star-inserted > span").innerText
+}
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Account"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-parent.mat-column-parent.ng-star-inserted > span").innerText
+}
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Organisation name"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.cdk-column-parent.mat-column-parent.ng-star-inserted > span").innerText
+}
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Integrations with reporting data"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.ic-icon-cell.cdk-column-feature0.mat-column-feature0.ng-star-inserted > span").innerText
+}
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Integrations with Cost Data"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.ic-icon-cell.cdk-column-feature0.mat-column-feature0.ng-star-inserted > span").innerText
+} 
+
+for(i=0;i < document.querySelectorAll('mat-row').size(); i++) {
+IntegrationDetails[0]["Integrations with Remarketing Lists"] = document.querySelector("#cdk-overlay-1 > mdx-slider-container > ic-product-pair-table > div > div > mat-table > mat-row:nth-child(2) > mat-cell.mat-cell.cdk-cell.mdx-table-cell.mdx-table-cell-action.ic-icon-cell.cdk-column-feature2.mat-column-feature2.ng-star-inserted > span").innerText
+}
+
+copy(IntegrationDetails)
